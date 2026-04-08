ffd9832af43e525b971821ddeb9194fe4196f7e5
juyeon320 <ju010320@pukyong.ac.kr>
2026-04-08 17:25:42 +0900

    requirement.txt

--- app.py
+++ app.py
@@ -162,4 +162,9 @@ def run_pipeline():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port, debug=True)
+
+#if __name__ == "__main__":
+#   app.run(debug=True)
+    
